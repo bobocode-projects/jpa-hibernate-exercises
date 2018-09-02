9b8cb0b0fea2dc44cdbea673aaaca024499edd37
tboychuk <tarass.boychuk@gmail.com>
2018-09-03 00:20:26 +0300

    Add new AccountDao exercise

---
+++ account-dao-exercise/src/main/java/com/bobocode/dao/AccountDaoImpl.java
@@ -0,0 +1,45 @@
+package com.bobocode.dao;
+
+import com.bobocode.model.Account;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class AccountDaoImpl implements AccountDao {
+    private EntityManagerFactory emf;
+
+    public AccountDaoImpl(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public void save(Account account) {
+        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+    }
+
+    @Override
+    public Account findById(Long id) {
+        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+    }
+
+    @Override
+    public Account findByEmail(String email) {
+        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+    }
+
+    @Override
+    public List<Account> findAll() {
+        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+    }
+
+    @Override
+    public void update(Account account) {
+        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+    }
+
+    @Override
+    public void remove(Account account) {
+        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+    }
+}
+
