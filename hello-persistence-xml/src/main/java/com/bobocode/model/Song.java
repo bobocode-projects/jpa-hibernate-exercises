386265ca51b728dcb1d8dd791d10e8af8f570523
tboychuk <tarass.boychuk@gmail.com>
2018-09-03 14:55:43 +0300

    Create new exercise "Hello JPA entity"

--- hello-persistence-xml/src/main/java/com/bobocode/model/Song.java
+++ hello-persistence-xml/src/main/java/com/bobocode/model/Song.java
@@ -1,7 +1,14 @@
 package com.bobocode.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "song")
 public class Song {
