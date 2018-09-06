47258de519952a01edbe6ad2420237180f54bbb9
tboychuk <tarass.boychuk@gmail.com>
2018-09-06 20:47:50 +0300

    Complete "Authors & Books" exercise

--- author-book/src/main/java/com/bobocode/model/Author.java
+++ author-book/src/main/java/com/bobocode/model/Author.java
@@ -1,50 +1,61 @@
 package com.bobocode.model;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * todo:
- * - implement no arguments constructor
- * - implement getters and setters for all fields
- * - implement equals() based on identifier field
- * - implement hashCode() that return constant value 31
- * - make setter for field {@link Author#books} private
- * - initialize field {@link Author#books} as new {@link HashSet}
- * - implement a helper {@link Author#addBook(Book)} that establishes a relation on both sides
- * - implement a helper {@link Author#removeBook(Book)} that drops a relation on both sides
- * <p>
- * - configure JPA entity
- * - specify table name: "author"
- * - configure auto generated identifier
- * - configure mandatory column "first_name" for field {@link Author#firstName}
- * - configure mandatory column "last_name" for field {@link Author#lastName}
- * <p>
- * - configure many-to-many relation between {@link Author} and {@link Book}
- * - configure cascade operations for this relations {@link CascadeType#PERSIST} and {@link CascadeType#MERGE}
- * - configure link (join) table "author_book"
- * - configure foreign key column "book_id" references book table
- * - configure foreign key column "author_id" references author table
- */
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
-    private Set<Book> books;
+
+    @Setter(AccessLevel.PRIVATE)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "author_book",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private Set<Book> books = new HashSet<>();
 
     public void addBook(Book book) {
-        throw new UnsupportedOperationException("Are you kidding me?");
+        books.add(book);
+        book.getAuthors().add(this);
     }
 
     public void removeBook(Book book) {
-        throw new UnsupportedOperationException("Are you kidding me?");
+        books.remove(book);
+        book.getAuthors().remove(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
     }
 }
+
