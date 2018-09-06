435bb5f7d24307acf93e54e0d737d678a460dbe9
tboychuk <tarass.boychuk@gmail.com>
2018-09-06 22:30:06 +0300

    Add new exercise "Company products"

---
+++ company-products/src/main/java/com/bobocode/model/Product.java
@@ -0,0 +1,28 @@
+package com.bobocode.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * todo:
+ * - implement no arguments constructor
+ * - implement getters and setters for all fields
+ * - implement equals() and hashCode() based on identifier field
+ * <p>
+ * - configure JPA entity
+ * - specify table name: "product"
+ * - configure auto generated identifier
+ * - configure mandatory column "name" for field {@link Product#name}
+ * <p>
+ * - configure many-to-one relation between {@link Product} and {@link Company}
+ * - configure foreign key column "company_id" references company table
+ */
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    private Long id;
+    private String name;
+    private Company company;
+}
