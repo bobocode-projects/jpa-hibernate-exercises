a35e66d79d32d159e8c2876c9b2ce5c0587af751
tboychuk <tarass.boychuk@gmail.com>
2018-09-06 12:38:58 +0300

    Add todo comments

--- employee-profile/src/main/java/com/bobocode/model/Employee.java
+++ employee-profile/src/main/java/com/bobocode/model/Employee.java
@@ -4,6 +4,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * todo:
+ * - implement not argument constructor
+ * - implement getters and setters
+ * - implement equals and hashCode based on identifier field
+ *
+ * - configure JPA entity
+ * - specify table name: "employee"
+ * - configure auto generated identifier
+ * - configure not nullable columns: email, firstName, lastName
+ *
+ * - initialize field comments
+ * - map relation between {@link Employee} and {@link EmployeeProfile} on the child side
+ */
 @NoArgsConstructor
 @Getter
 @Setter
--- employee-profile/src/main/java/com/bobocode/model/EmployeeProfile.java
+++ employee-profile/src/main/java/com/bobocode/model/EmployeeProfile.java
@@ -4,6 +4,20 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * todo:
+ * - implement not argument constructor
+ * - implement getters and setters
+ * - implement equals and hashCode based on identifier field
+ *
+ * - configure JPA entity
+ * - specify table name: "employee_profile"
+ * - configure not nullable columns: position, department
+ *
+ * - map relation between {@link Employee} and {@link EmployeeProfile} using foreign_key column: "employee_id"
+ * - configure a derived identifier. E.g. map "employee_id" column should be also a primary key (id) for this entity
+ * - configure relation as mandatory (not optional)
+ */
 @NoArgsConstructor
 @Getter
 @Setter
