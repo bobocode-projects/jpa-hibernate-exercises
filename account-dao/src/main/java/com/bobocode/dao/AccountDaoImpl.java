ffc60b66b3e9e78004367a78baea488be56c3eff
shtramak <shtramak@gmail.com>
2018-11-24 13:35:35 +0200

    implement account-dao exercise

--- account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
+++ account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
@@ -1,9 +1,13 @@
 package com.bobocode.dao;
 
-import com.bobocode.model.Account;
-
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import com.bobocode.exception.AccountDaoException;
+import com.bobocode.model.Account;
 
 public class AccountDaoImpl implements AccountDao {
     private EntityManagerFactory emf;
@@ -14,32 +18,67 @@ public class AccountDaoImpl implements AccountDao {
 
     @Override
     public void save(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        executeEntityManagerRequest(entityManager -> entityManager.persist(account));
     }
 
     @Override
     public Account findById(Long id) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        return resultListFromEntityManagerRequest(entityManager -> entityManager.find(Account.class, id));
     }
 
     @Override
     public Account findByEmail(String email) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        return resultListFromEntityManagerRequest(entityManager -> entityManager
+                .createQuery("select a from Account a where a.email=:email", Account.class)
+                .setParameter("email", email)
+                .getResultList()
+                .get(0));
     }
 
     @Override
     public List<Account> findAll() {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        return resultListFromEntityManagerRequest(entityManager -> entityManager
+                .createQuery("from Account ", Account.class)
+                .getResultList());
     }
 
     @Override
     public void update(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        executeEntityManagerRequest(entityManager -> entityManager.merge(account));
     }
 
     @Override
     public void remove(Account account) {
-        throw new UnsupportedOperationException("I don't wanna work without implementation!"); // todo
+        executeEntityManagerRequest(entityManager -> {
+            Account mergedAccount = entityManager.merge(account);
+            entityManager.remove(mergedAccount);
+        });
+    }
+
+    private void executeEntityManagerRequest(Consumer<EntityManager> emConsumer) {
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+        try {
+            emConsumer.accept(entityManager);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw new AccountDaoException("EntityManager request wasn't executed!", e.getCause());
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    private <T> T resultListFromEntityManagerRequest(Function<EntityManager, T> emFunction) {
+        EntityManager entityManager = emf.createEntityManager();
+        try {
+            T resultList = emFunction.apply(entityManager);
+            return resultList;
+        } catch (Exception e) {
+            throw new AccountDaoException("EntityManager request wasn't executed!", e.getCause());
+        } finally {
+            entityManager.close();
+        }
     }
 }
 
