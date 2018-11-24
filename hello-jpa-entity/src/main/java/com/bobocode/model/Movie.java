062e6a18731266659149253da0ad8b5bba7aabe7
shtramak <shtramak@gmail.com>
2018-11-24 13:38:05 +0200

    implement hello-jpa-entity exercise

--- hello-jpa-entity/src/main/java/com/bobocode/model/Movie.java
+++ hello-jpa-entity/src/main/java/com/bobocode/model/Movie.java
@@ -1,5 +1,12 @@
 package com.bobocode.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,12 +21,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "movie")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "director", nullable = false)
     private String director;
 
+    @Column(name = "duration")
     private Integer durationSeconds;
 }
