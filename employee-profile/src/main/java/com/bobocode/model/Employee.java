3df84d072d3e60c003f40e97e74db78262fb87dd
shtramak <shtramak@gmail.com>
2018-11-26 18:04:01 +0200

    implement employee profile exercise

--- employee-profile/src/main/java/com/bobocode/model/Employee.java
+++ employee-profile/src/main/java/com/bobocode/model/Employee.java
@@ -1,5 +1,12 @@
 package com.bobocode.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,21 +16,29 @@ import lombok.Setter;
  * - implement no argument constructor
  * - implement getters and setters
  * - implement equals and hashCode based on identifier field
- *
+ * <p>
  * - configure JPA entity
  * - specify table name: "employee"
  * - configure auto generated identifier
  * - configure not nullable columns: email, firstName, lastName
- *
+ * <p>
  * - initialize field comments
  * - map unidirectional relation between {@link Employee} and {@link EmployeeProfile} on the child side
  */
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(of = "id")
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue
     private Long id;
+    @Column(name = "email", nullable = false)
     private String email;
+    @Column(name = "first_name", nullable = false)
     private String fistName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 }
--- employee-profile/src/main/java/com/bobocode/model/EmployeeProfile.java
+++ employee-profile/src/main/java/com/bobocode/model/EmployeeProfile.java
@@ -1,5 +1,14 @@
 package com.bobocode.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,20 +18,29 @@ import lombok.Setter;
  * - implement not argument constructor
  * - implement getters and setters
  * - implement equals and hashCode based on identifier field
- *
+ * <p>
  * - configure JPA entity
  * - specify table name: "employee_profile"
  * - configure not nullable columns: position, department
- *
+ * <p>
  * - map relation between {@link Employee} and {@link EmployeeProfile} using foreign_key column: "employee_id"
  * - configure a derived identifier. E.g. map "employee_id" column should be also a primary key (id) for this entity
  */
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "employee_profile")
 public class EmployeeProfile {
+    @Id
     private Long id;
+    @OneToOne
+    @JoinColumn(name = "employee_id")
+    @MapsId
     private Employee employee;
+    @Column(name = "position", nullable = false)
     private String position;
+    @Column(name = "department", nullable = false)
     private String department;
 }
